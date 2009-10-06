3cf81f0fda27c562a9a610b879b37c6e18e1e435
David E. Wheeler <david@justatheory.com>
2009-10-06 12:48:06 -0700

    Fixed the GitHub links for realz.

--- lib/DBIx/Connector.pm
+++ lib/DBIx/Connector.pm
@@ -759,10 +759,10 @@ Not sure yet if I want these to be public. I might kill them off.
 =head1 Support
 
 This module is stored in an open GitHub repository,
-L<http://github.com/theory/dbix->connector/tree/>. Feel free to fork and
+L<http://github.com/theory/dbix-connector/tree/>. Feel free to fork and
 contribute!
 
-Please file bug reports at L<http://github.com/theory/dbix->connectora/issues/>.
+Please file bug reports at L<http://github.com/theory/dbix-connector/issues/>.
 
 =head1 Authors
 
